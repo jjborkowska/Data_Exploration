bc1f1a5bd0f9870205d52300d6e86861fdc04217
Milosz-B <milosz.bar12@gmail.com>
2026-04-23 10:38:48 +0200

    Update urls.py

--- sportsapp/urls.py
+++ sportsapp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("aplikacja/", include("aplikacja.urls")),
+    path("", include("aplikacja.urls")),
     path('admin/', admin.site.urls),
 ]

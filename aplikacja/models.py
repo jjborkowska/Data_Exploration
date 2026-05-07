b3f75f5a8126e2dc25c681e049d088de48300e9c
jjborkowska <jjb.jjborkowska@gmail.com>
2026-05-07 10:28:14 +0200

    test commit - pools

--- aplikacja/models.py
+++ aplikacja/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.Charfield("a question", max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CHarField(max_length=200)

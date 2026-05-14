2766802da26d9220d4c1683842f22977b74f32a9
Milosz-B <milosz.bar12@gmail.com>
2026-05-14 11:14:01 +0200

    Update settings.py

--- sportsapp/settings.py
+++ sportsapp/settings.py
@@ -38,13 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind',
-    'theme',
 ]
 
-TAILWIND_APP_NAME = 'theme'
-
-NPM_BIN_PATH = 'C:/Program Files/nodejs/npm.cmd'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
